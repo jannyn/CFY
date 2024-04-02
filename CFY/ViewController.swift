307bfa2ab89ba39b7ac1c1928bd7765689998381
Janny <jannynguyen@unomaha.edu>
2024-04-02 18:38:17 -0500

    Initial Commit

---
+++ CFY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CFY
+//
+//  Created by Janny Nguyen on 4/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

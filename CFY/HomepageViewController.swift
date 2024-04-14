d849f5865bb67e47b488bc91c25b517666fadfa0
Janny <jannynguyen@unomaha.edu>
2024-04-14 16:22:13 -0500

    okay did a little oopsie but we're so back

--- CFY/HomepageViewController.swift
+++ CFY/HomepageViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var newsletter_button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 //balls
 

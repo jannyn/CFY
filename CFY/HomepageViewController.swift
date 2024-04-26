0c13115035cddf991c7e0985485d6b720ae8dd2d
jannyn <jannynguyen@unomaha.edu>
2024-04-25 19:06:21 -0500

    added constraints to the font size

--- CFY/HomepageViewController.swift
+++ CFY/HomepageViewController.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var newsletter_button: UIButton!
+    @IBOutlet weak var newsletter_label: UILabel!
+    @IBOutlet weak var feedback_label: UILabel!
+    @IBOutlet weak var materials_label: UILabel!
+    @IBOutlet weak var contact_label: UILabel!
+    
+    let relative_font_constant: CGFloat = 0.030
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        newsletter_label.font = newsletter_label.font.withSize(self.view.frame.height * relative_font_constant)
+        feedback_label.font = feedback_label.font.withSize(self.view.frame.height * relative_font_constant)
+        materials_label.font = materials_label.font.withSize(self.view.frame.height * relative_font_constant)
+
         
     }
 //balls
